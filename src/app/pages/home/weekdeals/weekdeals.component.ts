f8675a566e8ce07af9086ab219490041e8e0d81c
Nayem Hossain <nayemhossssain@gmail.com>
2023-07-06 00:38:26 +0600

    Week Deals Section added

---
+++ src/app/pages/home/weekdeals/weekdeals.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-weekdeals',
+  templateUrl: './weekdeals.component.html',
+  styleUrls: ['./weekdeals.component.scss']
+})
+export class WeekdealsComponent {
+
+}
