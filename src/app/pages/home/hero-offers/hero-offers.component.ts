2b977add84edbd8557d1638613c61eda9d284c46
webfaisalbd <web.faisal.bd@gmail.com>
2023-07-09 08:01:53 +0600

    services in hero section  added

---
+++ src/app/pages/home/hero-offers/hero-offers.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-offers',
+  templateUrl: './hero-offers.component.html',
+  styleUrls: ['./hero-offers.component.scss']
+})
+export class HeroOffersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -3,16 +3,22 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
-import { WeekdelsComponent } from './weekdels/weekdels.component';
+// import { WeekdelsComponent } from './weekdels/weekdels.component';
 import { WeekdealsComponent } from './weekdeals/weekdeals.component';
+import { HeroServicesComponent } from './hero-services/hero-services.component';
+import { HeroBannerComponent } from './hero-banner/hero-banner.component';
+import { HeroOffersComponent } from './hero-offers/hero-offers.component';
 
 
 
 @NgModule({
   declarations: [
     HomeComponent,
-    WeekdelsComponent,
+    // WeekdelsComponent,
     WeekdealsComponent,
+    HeroServicesComponent,
+    HeroBannerComponent,
+    HeroOffersComponent,
 
   ],
   imports: [
